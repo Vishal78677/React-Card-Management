b4d72fed7ec2f417d6d5ef8f31516df1b3dd1769
Vishal7867 <vishaldantani777@gmail.com>
2025-02-14 16:52:06 +0530

    style: scss adds, code style changes, global variables adds

---
+++ src/constants/constant.ts
@@ -0,0 +1,65 @@
+
+
+
+export const headerHeight = {
+    default: 'h-12',
+    sm: 'sm:h-16',
+    md: 'md:h-20',
+    lg: 'lg:h-24',
+    xl: 'xl:h-28'
+}
+
+
+export const footerHeight = {
+    default: 'h-10',
+    sm: 'sm:h-12',
+    md: 'md:h-16',
+    lg: 'lg:h-20',
+    xl: 'xl:h-24'
+}
+
+export const marginX = {
+    default: 'mx-4',     
+    sm: 'sm:mx-6',        
+    md: 'md:mx-8',        
+    lg: 'lg:mx-10',       
+    xl: 'xl:mx-12',       
+}
+
+
+export const bodyMarginY = {
+    default: 'my-4',     
+    sm: 'sm:my-6',        
+    md: 'md:my-8',        
+    lg: 'lg:my-10',       
+    xl: 'xl:my-12',   
+}
+
+export const bodyPaddingX = {
+    default: 'px-4',     
+    sm: 'sm:px-6',        
+    md: 'md:px-8',        
+    lg: 'lg:px-10',       
+    xl: 'xl:px-12',   
+}
+
+
+export const fontWeight = {
+    default: "font-medium",
+    light: "font-light", // 300
+    normal: "font-normal", // 400,
+    medium: "font-medium", // 500,
+    semiBold: "font-semibold", // 600
+    bold: "font-bold", //700,
+
+}
+
+export const fontSize = {
+ 
+    default: "text-xs",
+    sm: "text-xs",
+    md: "text-sm",
+    lg: "text-base",
+    xl: "text-xl "
+
+}

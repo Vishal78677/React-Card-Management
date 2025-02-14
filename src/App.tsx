0acea225d94edc7058f337503924a161f0738fba
Vishal7867 <vishaldantani777@gmail.com>
2025-02-15 01:24:35 +0530

    feat(component): setup heder and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import { useState } from "react";
+import "./App.scss";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import "swiper/swiper-bundle.css";
 
 function App() {
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+
+  const handleOpenSidebar = () => setIsOpenSidebar(true);
+  const handleCloseSidebar = () => setIsOpenSidebar(false);
+
   return (
-    <>
-      <div className="">hello</div>
-    </>
+    <div>
+      <Header handleOpenSideBar={handleOpenSidebar} />
+      <Main
+        isOpenSidebar={isOpenSidebar}
+        handleCloseSidebar={handleCloseSidebar}
+      />
+    </div>
   );
 }
 
